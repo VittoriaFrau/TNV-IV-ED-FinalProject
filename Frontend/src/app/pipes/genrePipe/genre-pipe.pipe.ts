77fafa86862bce86a0b8cc62419036e5e8337d73
vittoriafrau <vittoriafrau1995f@gmail.com>
2021-09-28 13:12:07 +0200

    Error message in filter handler

--- Frontend/src/app/pipes/genrePipe/genre-pipe.pipe.ts
+++ Frontend/src/app/pipes/genrePipe/genre-pipe.pipe.ts
@@ -8,6 +8,7 @@ export class GenrePipePipe implements PipeTransform {
 
   transform(movies: MovieData [], genre: string): Array<MovieData> {
     let myArray =[];
+    if(!movies) return;
     for(let i=0;i<movies.length;i++){
       if(movies[i].genre==genre){
         myArray.push(movies[i]);
